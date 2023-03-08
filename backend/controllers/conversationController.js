d678be62b9d4cc70e7f8df2454bec2249a1eac74
Nguyễn Văn An <nva6112002@gmail.com>
2023-03-09 01:31:56 +0700

    Write api for user controller

--- backend/controllers/conversationController.js
+++ backend/controllers/conversationController.js
@@ -35,7 +35,7 @@ export const sendMessageToConversation = async (req, res, next) => {
 export const deleteConversation = async (req, res, next) => {
     try {
         const rs = await Conversation.findByIdAndDelete(req.params.conversationId);
-        
+
     } catch (error) {
         next(error);
     }
@@ -44,7 +44,7 @@ export const deleteConversation = async (req, res, next) => {
 export const getMessageFromConversation = async (req, res, next) => {
     try {
         const conversation = await Conversation.findById(req.params.conversationId)
-        res.status(200).json(conversation )
+        res.status(200).json(conversation)
     } catch (error) {
         next(error)
     }
@@ -54,18 +54,18 @@ export const getAllConversations = async (req, res, next) => {
     try {
         const userId = req.params.userId;
         const list = await Conversation.find({
-            participants: { $in: [userId]}
+            participants: { $in: [userId] }
         }).populate({
             path: 'participants',
             select: 'name avatar _id'
-          });
+        });
         console.log(list)
         const data = list.map(conversation => {
             const lastIndex = conversation.message.length - 1;
             const lastestMsg = conversation.message[lastIndex];
-            const {message, ...others} = conversation.toObject();
-            return {lastestMsg, ...others };
-          });
+            const { message, ...others } = conversation.toObject();
+            return { ...others, lastestMsg };
+        });
         console.log("data", data._doc)
         res.status(200).json(data);
     } catch (error) {
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,25 +5,41 @@ import User from "../models/userModel.js";
 import Conversation from "../models/conversationModel.js";
 
 export const sendFriendRequest = async (req, res, next) => {
+    const session = await mongoose.startSession();
     try {
         // TODO: chưa check id người gửi
-        const friend = await User.findOne({ friendRequest: req.body.senderId })
-        if (friend !== null) {
-            res.status(404).json({ success: false, message: `Already sent a friend request before` })
+        session.startTransaction();
+
+        const sender = await User.updateOne(
+            { _id: req.body.senderId },
+            { $push: { invitationSent: req.body.receiverId } }
+        );
+        // console.log("🚀 ~ file: userController.js:17 ~ sendFriendRequest ~ sender:", sender)
+        if (sender.matchedCount === 0) {
+            throw createError(404, "Sender not found");
         }
+        // const receiver = await User.findOne({ _id: req.body.receiverId });
+        // if (receiver !== null) {
+        //     res.status(404).json({ success: false, message: `Already sent a friend request before` })
+        // }
 
         await User.updateOne(
             { _id: req.body.receiverId },
             { $push: { friendRequest: req.body.senderId } }
         );
+        session.commitTransaction();
         res.status(200).json({ success: true, message: "Invitation sent successfully" })
 
     } catch (error) {
+        await session.abortTransaction();
         next(error);
+    }
+    finally {
+        session.endSession();
     }
 }
 
-export const addNewFriend = async (req, res, next) => {
+export const acceptNewFriend = async (req, res, next) => {
     let session = await mongoose.startSession();
     try {
         const friend = await User.findOne({ friendRequest: req.body.senderId })
@@ -45,7 +61,8 @@ export const addNewFriend = async (req, res, next) => {
             await User.updateOne(
                 { _id: req.body.senderId },
                 {
-                    $push: { friendsList: req.body.receiverId }
+                    $push: { friendsList: req.body.receiverId },
+                    $pull: { invitationSent: req.body.receiverId }
                 }
             );
 
@@ -62,7 +79,28 @@ export const addNewFriend = async (req, res, next) => {
 
     } catch (error) {
         await session.abortTransaction();
+        next(error);
+    }
+    finally {
+        session.endSession();
+    }
+}
 
+export const rejectNewFriend = async (req, res, next) => {
+    const session = await mongoose.startSession();
+    try {
+        session.startTransaction();
+        await User.updateOne(
+            { _id: req.body.userId },
+            { $pull: { friendRequest: req.body.friendId } }
+        );
+        await User.invitationSent(
+            { _id: req.body.friendId },
+            { $pull: { friendRequest: req.body.userId } }
+        );
+        session.commitTransaction();
+    } catch (error) {
+        await session.abortTransaction();
         next(error);
     }
     finally {
@@ -73,10 +111,10 @@ export const addNewFriend = async (req, res, next) => {
 export const getFriendsList = async (req, res, next) => {
     try {
         const list = await User.findById(req.params.userId)
-            .populate('friendsList', 'name avatar -_id')
+            .populate('friendsList', 'name avatar')
             .select({ friendsList: 1, _id: 0 });
         console.log(list);
-        res.status(200).json({ success: true, data: list.friendsList });
+        res.status(200).json(list.friendsList);
     } catch (error) {
         next(error)
     }
@@ -137,15 +175,54 @@ export const getProfileByEmail = async (req, res, next) => {
     }
 }
 
+export const getProfileById = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.params.userId);
+        // TODO: gắn thêm flag để biết user này có phải là bạn bè không
+        if (user === null) {
+            res.status(404).json(null);
+            return;
+        }
+        const { password, ...others } = user._doc;
+        res.status(200).json({ ...others });
+    } catch (error) {
+        next(error);
+    }
+}
+
+// export const getProfileMyFriend = async (req, res, next) => {
+//     try {
+//     } catch (error) {
+//         next(error);
+//     }
+// }
+
 export const getFriendsRequestList = async (req, res, next) => {
     try {
         const list = await User.findById(req.params.userId)
-                                .populate({
-                                    path: 'friendRequest',
-                                    select: '_id name'
-                                })
-                                .select({_id: 0, friendRequest: 1})
-        res.status(200).json({...list});
+            .populate({
+                path: 'friendRequest',
+                select: '_id name'
+            })
+            .select({ _id: 0, friendRequest: 1 })
+        const { friendRequest } = { ...list._doc }
+        res.status(200).json(friendRequest);
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const getListOfInvitationsSent = async (req, res, next) => {
+    try {
+        const list = await User.findById(req.params.userId)
+            .populate({
+                path: 'invitationSent',
+                select: '_id name avatar'
+            })
+            .select({ _id: 0, invitationSent: 1 });
+
+        const { invitationSent } = list._doc;
+        res.status(200).json(invitationSent);
     } catch (error) {
         next(error);
     }
