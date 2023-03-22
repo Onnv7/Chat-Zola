65c019f7415bd61d24d13a4f12b0b246a5336d63
fox <phurong1@gmail.com>
2023-03-22 10:15:24 +0700

    rest profile, friend

--- frontend/src/Components/Chat/Chat.jsx
+++ frontend/src/Components/Chat/Chat.jsx
@@ -1,149 +1,146 @@
-import React, {useContext, useState, useEffect, useRef, useLayoutEffect} from 'react';
+import React, { useContext, useState, useEffect, useRef, useLayoutEffect } from 'react';
 import { io } from 'socket.io-client';
 
 import './chat.scss';
 import { AuthContext } from '../../Contexts/AuthContext.js';
-import axios from "../../Hooks/axios.js";
+import axios from '../../Hooks/axios.js';
 
-const Chat = ({conversation, handleLatestMsg}) => {
+const Chat = ({ conversation, handleLatestMsg }) => {
     const { user } = useContext(AuthContext);
-    
+
     const containerRef = useRef(null);
     // const [conv, setConv] = useState(conversation);
-    const [isLoadingOldMsg, setIsLoadingOldMsg] = useState(false)
+    const [isLoadingOldMsg, setIsLoadingOldMsg] = useState(false);
     const conv = useRef(conversation);
-    const [text, setText] = useState("");
+    const [text, setText] = useState('');
     const [skip, setSkip] = useState(0);
     const [messages, setMessages] = useState([]);
     const [arrivalMessage, setArrivalMessage] = useState(null);
     const socket = useRef();
 
     useEffect(() => {
-        socket.current = io("ws://localhost:8900");
-        socket.current.on("getMessage", (data) => {
-            if(conv.current?.id === data.conversationId)
-                setArrivalMessage(data?.message)
+        socket.current = io('ws://localhost:8900');
+        socket.current.on('getMessage', (data) => {
+            if (conv.current?.id === data.conversationId) setArrivalMessage(data?.message);
             handleLatestMsg(data);
         });
     }, []);
 
     useEffect(() => {
-        if(isLoadingOldMsg === false) {
+        if (isLoadingOldMsg === false) {
             const container = containerRef.current;
             container.scrollTop = container.scrollHeight;
-        }
-        else 
-        {
+        } else {
             const container = containerRef.current;
             container.scrollTop = 20;
         }
-    }, [messages])
-    
+    }, [messages]);
 
     useEffect(() => {
-        const fetchFirstMessages =async (conversationId) => {
-            if(!conv.current)
-            return
-            const { data } = await axios.get(`/conversation/messages/${conversationId}/0`)
+        const fetchFirstMessages = async (conversationId) => {
+            if (!conv.current) return;
+            const { data } = await axios.get(`/conversation/messages/${conversationId}/0`);
             setSkip(10);
-            return data
-        }
+            return data;
+        };
         conv.current = conversation;
-        fetchFirstMessages(conversation?.id)
-                    .then(res => {setMessages(res);setIsLoadingOldMsg(false)})
-                    
-    }, [conversation])
-    
-    useEffect(() => {     
-        if(conversation === undefined) 
-        return;
-        if (arrivalMessage) 
-            setMessages((prev) => {
-                return [...prev, arrivalMessage]
-            })
-    }, [arrivalMessage])
+        fetchFirstMessages(conversation?.id).then((res) => {
+            setMessages(res);
+            setIsLoadingOldMsg(false);
+        });
+    }, [conversation]);
 
     useEffect(() => {
-        socket.current.emit("addUser", user._id)
-        socket.current.on("getUsers", users => console.log(users))
-    }, [user])
-    const handleClickSendMessage = async (conversationId) => {
-        if(text.trim() === "")
-            return;
-        const sentAt = Date.now()
-        await axios.post(`/conversation/send-messages/${conversationId}`, {
-            sender: user._id,
-            message: text,
-            sentAt: sentAt
-        })
-        .then((res) => {
-            setMessages(prev => [...prev, {
-                sender: user._id,
-                content: text
-            }])
-            handleLatestMsg({
-                conversationId: conversationId,
-                message: {
-                    sender: user._id,
-                    content: text,
-                    sentAt: new Date(sentAt)
-                }
+        if (conversation === undefined) return;
+        if (arrivalMessage)
+            setMessages((prev) => {
+                return [...prev, arrivalMessage];
             });
-            return res;
-        })
-        .then((res) => {
-            socket.current.emit("sendMessage", {
-                conversationId: conversationId,
-                senderId: user._id,
-                receiverId: conversation.friend._id,
-                message: {
-                    sender: user._id,
-                    content: text,
-                    sentAt: new Date(sentAt)
-                }
+    }, [arrivalMessage]);
+
+    useEffect(() => {
+        socket.current.emit('addUser', user._id);
+        socket.current.on('getUsers', (users) => console.log(users));
+    }, [user]);
+    const handleClickSendMessage = async (conversationId) => {
+        if (text.trim() === '') return;
+        const sentAt = Date.now();
+        await axios
+            .post(`/conversation/send-messages/${conversationId}`, {
+                sender: user._id,
+                message: text,
+                sentAt: sentAt,
             })
-            setText("");
-            setSkip(prev => prev + 1);
-            setIsLoadingOldMsg(false);
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-    }
-    
+            .then((res) => {
+                setMessages((prev) => [
+                    ...prev,
+                    {
+                        sender: user._id,
+                        content: text,
+                    },
+                ]);
+                handleLatestMsg({
+                    conversationId: conversationId,
+                    message: {
+                        sender: user._id,
+                        content: text,
+                        sentAt: new Date(sentAt),
+                    },
+                });
+                return res;
+            })
+            .then((res) => {
+                socket.current.emit('sendMessage', {
+                    conversationId: conversationId,
+                    senderId: user._id,
+                    receiverId: conversation.friend._id,
+                    message: {
+                        sender: user._id,
+                        content: text,
+                        sentAt: new Date(sentAt),
+                    },
+                });
+                setText('');
+                setSkip((prev) => prev + 1);
+                setIsLoadingOldMsg(false);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+
     useEffect(() => {
         const fetchMessages = async () => {
-            if(!conversation)
-            return
-            const { data } = await axios.get(`/conversation/messages/${conversation.id}/${skip}`)
+            if (!conversation) return;
+            const { data } = await axios.get(`/conversation/messages/${conversation.id}/${skip}`);
             // setSkip(prevSkip => prevSkip + 10);
-            return data
-        }
+            return data;
+        };
         const handleScroll = () => {
             const container = containerRef.current;
-            if (container.scrollTop === 0)  {
-                console.log("????????", skip, messages?.length)
-                fetchMessages()
-                .then(res => {
-                    if(res.length !== 0) 
-                    {
-                        setMessages(prevMessages => [...res, ...prevMessages]);
-                        setSkip(prevSkip => prevSkip + 10);
+            if (container.scrollTop === 0) {
+                console.log('????????', skip, messages?.length);
+                fetchMessages().then((res) => {
+                    if (res.length !== 0) {
+                        setMessages((prevMessages) => [...res, ...prevMessages]);
+                        setSkip((prevSkip) => prevSkip + 10);
                         setIsLoadingOldMsg(true);
                     }
-                })
+                });
             }
         };
         const container = containerRef.current;
         // if(container !== null)
-            container.addEventListener('scroll', handleScroll);
+        container.addEventListener('scroll', handleScroll);
         return () => container.removeEventListener('scroll', handleScroll);
     }, [conversation, skip]);
 
-    
-    
+    const inputFile = useRef(null);
 
-    
+    const openFolder = () => {
+        inputFile.current.click();
+    };
+
     return (
         <div className="chat">
             <div className="chat-container">
@@ -159,38 +156,45 @@ const Chat = ({conversation, handleLatestMsg}) => {
                     </div>
                 </div>
                 <div className="chat-view">
-                    <div className="message-view" 
-                        ref={containerRef} 
-                        style={{ overflowY: 'scroll', height: '400px' }}>
-                        {messages && messages.map((message) => {
-                                if(message.sender !== user._id)
-                                {
-                                    return (<div className="YourMessage">
-                                                <img src="../Img/Avatar1.png" alt="" />
-                                                <span>{message.content}</span>
-                                            </div>)
-                                }
-                                return (<div className="MyMessage">
+                    <div className="message-view" ref={containerRef} style={{ overflowY: 'scroll', height: '400px' }}>
+                        {messages &&
+                            messages.map((message) => {
+                                if (message.sender !== user._id) {
+                                    return (
+                                        <div className="YourMessage">
+                                            <img src="../Img/Avatar1.png" alt="" />
                                             <span>{message.content}</span>
-                                            <img src="../Img/Avatar.png" alt="" />
-                                        </div>)
-                        })}
+                                        </div>
+                                    );
+                                }
+                                return (
+                                    <div className="MyMessage">
+                                        <span>{message.content}</span>
+                                        <img src="../Img/Avatar.png" alt="" />
+                                    </div>
+                                );
+                            })}
                     </div>
                     <div className="chat-box">
                         <div className="chat-toolbar">
                             <i className="fa-light fa-face-smile-beam"></i>
-                            <i className="fa-light fa-image"></i>
+                            <i onClick={openFolder} className="fa-light fa-image"></i>
+                            <input type="file" id="file" ref={inputFile} style={{ display: 'none' }} />
                         </div>
                         <div className="chat-inputBox">
                             <div className="chat-input">
-                                <input type="text" 
-                                        placeholder="Nhập tin nhắn" 
-                                        value={text} 
-                                        onChange={(e) => {
-                                            setText(e.target.value)
-                                        }}/>
-                                <i className="fa-solid fa-paper-plane-top"
-                                    onClick={(e) => handleClickSendMessage(conversation.id)}></i>
+                                <input
+                                    type="text"
+                                    placeholder="Nhập tin nhắn"
+                                    value={text}
+                                    onChange={(e) => {
+                                        setText(e.target.value);
+                                    }}
+                                />
+                                <i
+                                    className="fa-solid fa-paper-plane-top"
+                                    onClick={(e) => handleClickSendMessage(conversation.id)}
+                                ></i>
                                 <i className="fa-solid fa-thumbs-up"></i>
                             </div>
                         </div>
--- frontend/src/Components/FriendInvite/FriendReceive.jsx
+++ frontend/src/Components/FriendInvite/FriendReceive.jsx
@@ -1,20 +1,73 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import { AuthContext } from '../../Contexts/AuthContext';
+import axios from '../../Hooks/axios';
 
-const FriendReceive = () => {
+const FriendReceive = ({ setIdr }) => {
+    const { user } = useContext(AuthContext);
+    const [list, setList] = useState();
+    const [active, setActive] = useState('');
+    useEffect(() => {
+        const fetchData = async () => {
+            const { data } = await axios.get(`/user/get-list-friend-requests/${user._id}`);
+            setList(data);
+        };
+        fetchData();
+    }, [list]);
+    const handleClick = (id) => {
+        setIdr(id);
+        setActive(id);
+    };
+    const handleAccept = async (id) => {
+        try {
+            await axios.post('/user/accept-new-friend', {
+                receiverId: user._id,
+                senderId: id,
+            });
+            // if (list.length > 0) {
+            //     setList(list.length - 1);
+            // } else setList(null);
+            setIdr(null);
+            toast.success('Kết bạn thành công');
+        } catch (err) {
+            toast.error(err.message);
+        }
+    };
+    const handleUnsend = async (id) => {
+        try {
+            await axios.post('/user/unsend-friend-request', {
+                receiverId: user._id,
+                senderId: id,
+            });
+            setIdr(null);
+            toast.success('Hủy lời mời thành công');
+        } catch (err) {
+            toast.error(err.message);
+        }
+    };
     return (
         <div className="friendInvite-List">
-            <div className="myFriend-item active-box">
-                <div className="myFriend-info">
-                    <img src="../Img/Avatar1.png" alt="" />
-                    <span>Friend A</span>
-                    <i class="fa-regular fa-mars"></i>
-                    <i class="fa-regular fa-venus hide"></i>
+            {list?.map((item) => (
+                <div
+                    key={item._id}
+                    className={active === item._id ? 'myFriend-item active-box' : 'myFriend-item'}
+                    onClick={() => handleClick(item._id)}
+                >
+                    <div className="myFriend-info">
+                        <img src="../Img/Avatar1.png" alt="" />
+                        <span>{item.name}</span>
+                        {/* {item.gender === 'male' ? (
+                            <i class="fa-regular fa-mars"></i>
+                        ) : (
+                            <i class="fa-regular fa-venus"></i>
+                        )} */}
+                    </div>
+                    <div className="myFriend-icon">
+                        <i onClick={() => handleAccept(item._id)} className="fa-solid fa-user-plus"></i>
+                        <i onClick={() => handleUnsend(item._id)} className="fa-solid fa-circle-xmark"></i>
+                    </div>
                 </div>
-                <div className="myFriend-icon">
-                    <i class="fa-solid fa-user-plus"></i>
-                    <i class="fa-solid fa-circle-xmark"></i>
-                </div>
-            </div>
+            ))}
         </div>
     );
 };
